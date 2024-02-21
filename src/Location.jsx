8c4594485432919d452bf824f3904a7ea9cdfd88
bburciaga <burciagab16@gmail.com>
2024-02-21 13:18:40 -0800

    updated thing

--- src/Location.jsx
+++ src/Location.jsx
@@ -31,7 +31,7 @@ function Box(props) {
   return (
             <mesh
               rotation={[
-                props.rotation.alpha, 0, 0
+                props.rotation.gamma, 0, 0
               ]}
               position={props.position}
               ref={meshRef}
