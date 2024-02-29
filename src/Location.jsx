d79dd9317eecd12e17bee72633432e27e16237c5
bburciaga <burciagab16@gmail.com>
2024-02-29 10:40:26 -0800

    updated thing

--- src/Location.jsx
+++ src/Location.jsx
@@ -29,7 +29,7 @@ function Box(props) {
   }, [meshRef])
 
   useFrame((state, delta) => (meshRef.current.rotation.z += props.rotation.gamma)) // Rotation in y axis
-  useFrame((state, delta) => (meshRef.current.position.x = props.rotation.beta))
+  useFrame((state, delta) => (meshRef.current.position.x += props.rotation.beta))
         
   return (
     <mesh
